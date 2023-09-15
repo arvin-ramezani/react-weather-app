72f5d91859d54a93d2738aac78dee435e5a91d9b
Arvin Ramezani <arvin.ramezani6@gmail.com>
2023-09-15 21:27:49 +0330

    Add vitest to project, and write first testing test suit for 'Weather.tsx' component

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite/';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
@@ -6,5 +7,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.ts'],
   },
 });
